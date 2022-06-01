c64214e0cbca6eaeae6058f2ac4c9171e7c0b023
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-06-01 17:19:51 +0800

    添加 generic 类

--- KotlinCodePractice/src/main/kotlin/example/generics/Aquarium.kt
+++ KotlinCodePractice/src/main/kotlin/example/generics/Aquarium.kt
@@ -16,12 +16,56 @@ class LakeWater : WaterSupply(true) {
     }
 }
 
-class Aquarium<T>(val waterSupply: T)
+class Aquarium<T: WaterSupply>(val waterSupply: T) {
+    fun addWater() {
+        check(!waterSupply.needsProcessing) {
+            "water supply needs processing first"
+        }
+        println("adding water from $waterSupply")
+    }
+}
 
 fun genericExample() {
     val aquarium = Aquarium<TapWater>(TapWater())
-    val tapWaterAquarium = Aquarium(TapWater())
-    val stringAquarium = Aquarium("string")
-    println(stringAquarium.javaClass)
-    println(stringAquarium.waterSupply)
+    try {aquarium.addWater()}
+    catch (e: Exception){
+        println(e.localizedMessage)
+    }
+
+    val lakeWaterAquarium = Aquarium(LakeWater())
+    try {
+        lakeWaterAquarium.addWater()
+    } catch (e: Exception) {
+        println(e.stackTrace.toString())
+    }
+    lakeWaterAquarium.waterSupply.filter()
+    lakeWaterAquarium.addWater()
+
+    val tapWaterAquariumOut = AquariumOut(TapWater())
+    addItemTo(tapWaterAquariumOut)
+
+    val tapWaterCleaner = TapWaterCleaner()
+    val aquariumOut = AquariumOut(TapWater())
+    aquariumOut.addWater(tapWaterCleaner)
+}
+
+class AquariumOut<out T: WaterSupply>(val waterSupply: T) {
+    fun addWater(cleaner: Cleaner<T>) {
+        if (waterSupply.needsProcessing) {
+            cleaner.clean(waterSupply)
+        }
+        println("water added")
+    }
+}
+
+fun addItemTo(aquariumOut: AquariumOut<WaterSupply>) = println("item added")
+
+interface Cleaner<in T: WaterSupply> {
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalCleaners()
+    }
 }
