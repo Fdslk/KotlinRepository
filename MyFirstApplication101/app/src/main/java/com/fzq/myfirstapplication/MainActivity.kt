f50d99622b0d94a08b9d8b78f63aa7050dedb566
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-06-13 06:26:35 +0800

    修改app布局

--- MyFirstApplication101/app/src/main/java/com/fzq/myfirstapplication/MainActivity.kt
+++ MyFirstApplication101/app/src/main/java/com/fzq/myfirstapplication/MainActivity.kt
@@ -11,15 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+//        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.view_text)
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nicknameTextView = findViewById<TextView>(R.id.view_text)
+//        nicknameTextView.text = editText.text
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        nicknameTextView.visibility = View.VISIBLE
     }
 }
