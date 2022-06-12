ce8ad21a89000b414cf25b64d35fa82034320037
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-06-13 06:58:10 +0800

    添加用户交互

--- MyFirstApplication101/app/src/main/java/com/fzq/myfirstapplication/MainActivity.kt
+++ MyFirstApplication101/app/src/main/java/com/fzq/myfirstapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.fzq.myfirstapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,15 +13,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener { updateNickName(it) }
     }
 
     private fun addNickname(view: View) {
-//        val editText = findViewById<EditText>(R.id.nickname_edit)
-//        val nicknameTextView = findViewById<TextView>(R.id.view_text)
-//        nicknameTextView.text = editText.text
-//        editText.visibility = View.GONE
-//        view.visibility = View.GONE
-//        nicknameTextView.visibility = View.VISIBLE
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickName(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+        // Set the focus to the edit text.
+        editText.requestFocus()
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
