16d09cd59f4d31f9fce022e5887ef5bbf19198c9
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-06-23 09:06:08 +0800

    定义databinding

---
+++ MyFirstApplication101/README.md
@@ -0,0 +1,24 @@
+# First Application
+* different views
+* layout
+* event handler
+* data binding
+  * help app no need to initialize the variable each time, which can save time.
+  * findViewById() find view will create or recreate when it is called.
+  * It is a global technology for the whole app, like a reference.
+  * how to use it
+    * enable data binding in your **build.gradle** file
+    ```xml
+    android {
+      buildFeatures {
+        dataBinding true
+      }
+    }
+    ```
+    * sync your application
+    * move the namespace declaration into layout tag and just keep view properties
+    * declare a binding variable for binding the specific layout which you have defined a layout tag. Then using the following code to set data to data binding
+    ```kotlin
+            binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+     ```
+    * replace the findViewById to binding.idView
--- MyFirstApplication101/app/src/main/java/com/fzq/myfirstapplication/MainActivity.kt
+++ MyFirstApplication101/app/src/main/java/com/fzq/myfirstapplication/MainActivity.kt
@@ -5,39 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.fzq.myfirstapplication.databinding.ActivityMainBinding // created by the compiler specifically for this main activity.
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
-        findViewById<TextView>(R.id.nickname_text).setOnClickListener { updateNickName(it) }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneButton.setOnClickListener { addNickname(it) }
+        binding.nicknameText.setOnClickListener { updateNickName() }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.nicknameText.text = binding.nicknameEdit.text.toString()
+        binding.nicknameEdit.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
-    private fun updateNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val doneButton = findViewById<Button>(R.id.done_button)
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
-        view.visibility = View.GONE
+    private fun updateNickName() {
+        binding.nicknameEdit.visibility = View.VISIBLE
+        binding.doneButton.visibility = View.VISIBLE
+        binding.nicknameText.visibility = View.GONE
         // Set the focus to the edit text.
-        editText.requestFocus()
+        binding.nicknameEdit.requestFocus()
         // Show the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText, 0)
+        imm.showSoftInput(binding.nicknameEdit, 0)
     }
 }
