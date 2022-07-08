e6a7ff781eeac87e4f3f22086df5d9cbbd054889
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-07-08 17:02:57 +0800

    使用Observe检测viewModel数据变化

--- ViewModel/app/src/main/java/com/fzq/viewmodel/screen/game/GameFragment.kt
+++ ViewModel/app/src/main/java/com/fzq/viewmodel/screen/game/GameFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.fzq.viewmodel.R
@@ -39,11 +40,17 @@ class GameFragment : Fragment() {
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
-        updateScoreText()
-        updateWordText()
 
         binding.endGameButton.setOnClickListener { onEndGame() }
         // Inflate the layout for this fragment
+
+        viewModel.score.observe(viewLifecycleOwner, Observer {
+            newScore -> binding.scoreText.text = newScore.toString()
+        })
+
+        viewModel.word.observe(viewLifecycleOwner, Observer {
+                newWord -> binding.wordText.text = newWord
+        })
         return binding.root
     }
 
@@ -65,23 +72,10 @@ class GameFragment : Fragment() {
 
     private fun onSkip() {
         viewModel.onSkip()
-        updateWordText()
-        updateScoreText()
     }
 
     private fun onCorrect() {
         viewModel.onCorrect()
-        updateWordText()
-        updateScoreText()
     }
 
-    /** Methods for updating the UI **/
-
-    private fun updateWordText() {
-        binding.wordText.text = viewModel.word.value
-    }
-
-    private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.value.toString()
-    }
 }
