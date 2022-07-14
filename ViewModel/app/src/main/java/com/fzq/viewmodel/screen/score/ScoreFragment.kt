d62673db046692c57dfe9a8e238dfd66f0bbddcd
zengqiang.fang <zqfangmaster@163.com>
2022-07-15 07:29:54 +0800

    使用databing直接和viewModel绑定在ScoreFragment中

--- ViewModel/app/src/main/java/com/fzq/viewmodel/screen/score/ScoreFragment.kt
+++ ViewModel/app/src/main/java/com/fzq/viewmodel/screen/score/ScoreFragment.kt
@@ -37,6 +37,9 @@ class ScoreFragment : Fragment() {
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ScoreViewModel::class.java)
+
+        binding.scoreViewModel = viewModel
+
         viewModel.score.observe(viewLifecycleOwner, Observer {
             newScore -> binding.scoreText.text = newScore.toString()
         })
@@ -48,8 +51,6 @@ class ScoreFragment : Fragment() {
                 viewModel.onPlayAgainComplete()
             }
         })
-
-        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
 
         return binding.root
     }
